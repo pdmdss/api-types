7d8f37c1399cc4813677472426543fa7054fa041
soshi1822 <soshi1822@gmail.com>
2021-03-30 18:33:25 +0900

    BillingGetを変更

--- component/api/v2/billing.get.ts
+++ component/api/v2/billing.get.ts
@@ -9,23 +9,40 @@ export namespace APIV2BillingGet {
   export interface ResponseOk extends APIV2Head {
     status: 'ok';
     targetDate: string;
-    items: {
-      classification: string;
-      classificationName: string;
-      targetTime: {
-        start: string;
-        end: string | null;
+    billing: {
+      items: {
+        classification: string;
+        classificationName: string;
+        targetTime: {
+          start: string;
+          end: string | null;
+        }[];
+        amount: {
+          subtotal: number;
+        };
       }[];
       amount: {
-        subtotal: number;
+        total: number;
+        discount: number;
+        payment: number;
       };
-    }[];
-    amount: {
-      total: number;
-      discount: number;
-      payment: number;
+    }
+    payment?: {
+      status: 'paid' | 'unpaid' | 'failed' | 'partialFailure';
+      items: {
+        id: number;
+        time: string;
+        amount: {
+          paid: number;
+        }
+        type: 'card' | 'charge';
+      }[];
+      amount: {
+        total: number;
+        paid: number;
+        unpaid: number;
+      };
     };
-    paymentStatus?: 'paid' | 'unpaid' | 'error';
   }
 
   export type ResponseError = APIV2BaseError;
