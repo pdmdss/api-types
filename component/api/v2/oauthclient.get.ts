44a3b811f5d32013191175eb8a04210c8aba6afd
soshi1822 <soshi1822@gmail.com>
2021-04-09 21:51:45 +0900

    OAuthClientGetを変更

--- component/api/v2/oauthclient.get.ts
+++ component/api/v2/oauthclient.get.ts
@@ -8,28 +8,26 @@ export namespace APIV2OAuthClientGet {
 
   export interface ResponseOk extends APIV2Head {
     status: 'ok';
-    oauthClient: {
-      id: number;
-      client: {
-        id: string;
-        secret: string;
-        type: 'confidential' | 'public';
-        name: string;
-        logo: string | null;
-      };
-      redirectUris: string[];
-      allowed: {
-        responseTypes: ('code' | 'token')[];
-        grantTypes: ('authorization_code' | 'refresh_token' | 'client_credentials')[];
-      };
-      site: {
-        homePageUri: string;
-        termsUri: string;
-        policyUri: string;
-      };
-      contacts: string[];
-      createTime: string;
-    }[];
+    id: number;
+    client: {
+      id: string;
+      secret: string;
+      type: 'confidential' | 'public';
+      name: string;
+      logo: string | null;
+    };
+    redirectUris: string[];
+    allowed: {
+      responseTypes: ('code' | 'token')[];
+      grantTypes: ('authorization_code' | 'refresh_token' | 'client_credentials')[];
+    };
+    site: {
+      homePageUri: string;
+      termsUri: string;
+      policyUri: string;
+    };
+    contacts: string[];
+    createTime: string;
   }
 
   export type ResponseError = APIV2BaseError;
