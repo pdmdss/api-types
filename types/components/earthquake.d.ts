b8c8c19d16c26b55fb46cb9c75990337eaed7c79
soshi1822 <soshi1822@gmail.com>
2022-06-26 16:53:50 +0900

    update: Earthquake component を 電文型定義の型と合わせる
    Signed-off-by: soshi1822 <soshi1822@gmail.com>

--- types/components/earthquake.d.ts
+++ types/components/earthquake.d.ts
@@ -2,12 +2,12 @@ export namespace Earthquake {
   export type IntensityClass = '1' | '2' | '3' | '4' | '5-' | '5+' | '6-' | '6+' | '7';
   export type LpgmIntensityClass = '0' | '1' | '2' | '3' | '4';
 
-  export interface Coordinate {
-    latitude?: {
+  interface CoordinateNormal {
+    latitude: {
       text: string;
       value: string;
     };
-    longitude?: {
+    longitude: {
       text: string;
       value: string;
     };
@@ -16,16 +16,50 @@ export namespace Earthquake {
       unit: 'm';
       value: string;
     };
+    condition: never;
     geodeticSystem?: '日本測地系';
-    condition?: string;
   }
 
-  export interface EarthquakeHypocenterDepth {
+  interface CoordinateUnknown {
+    latitude: never;
+    longitude: never;
+    height: never;
+    geodeticSystem: never;
+    condition: '不明';
+  }
+
+  export type Coordinate = CoordinateNormal | CoordinateUnknown;
+
+
+  interface DepthBase {
     type: '深さ';
     unit: 'km';
     value: string | null;
-    condition?: 'ごく浅い' | '７００ｋｍ以上' | '不明';
+    condition: string | never;
   }
+
+  interface DepthNormal extends DepthBase {
+    value: string;
+    condition: never;
+  }
+
+  interface DepthShallow extends DepthBase {
+    value: '0';
+    condition: 'ごく浅い';
+  }
+
+  interface DepthDeep extends DepthBase {
+    value: '700';
+    condition: '７００ｋｍ以上';
+  }
+
+  interface DepthUnknown extends DepthBase {
+    value: null;
+    condition: '不明';
+  }
+
+  export type EarthquakeHypocenterDepth = DepthNormal | DepthShallow | DepthDeep | DepthUnknown;
+
 
   export interface EarthquakeHypocenter {
     code: string;
@@ -35,12 +69,26 @@ export namespace Earthquake {
     source?: string;
   }
 
-  export interface EarthquakeMagnitude {
+
+  interface MagnitudeBase {
     type: 'マグニチュード';
     unit: 'Mj' | 'M';
     value: string | null;
-    condition?: 'Ｍ不明' | 'Ｍ８を超える巨大地震';
+    condition: string | never;
   }
+
+  interface MagnitudeNormal extends MagnitudeBase {
+    value: string;
+    condition: never;
+  }
+
+  interface MagnitudeUnknown extends MagnitudeBase {
+    value: null;
+    condition: 'Ｍ不明' | 'Ｍ８を超える巨大地震';
+  }
+
+  export type EarthquakeMagnitude = MagnitudeNormal | MagnitudeUnknown;
+
 
   export interface Event {
     id: number;
