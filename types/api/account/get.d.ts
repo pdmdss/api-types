ccfb58851f30e0bf68feeacfeec7fb2a6b8004e0
soshi1822 <soshi1822@gmail.com>
2022-08-25 18:58:41 +0900

    fix: 綴りの修正
    Signed-off-by: soshi1822 <soshi1822@gmail.com>

--- types/api/account/get.d.ts
+++ types/api/account/get.d.ts
@@ -10,7 +10,7 @@ export namespace AccountGet {
 
   export interface Account {
     id: number;
-    type: 'customer' | 'business' | 'sub_account';
+    type: 'consumer' | 'business' | 'sub_account';
     mailAddress: string;
     twoFactorAuth: boolean;
     domicile: {
