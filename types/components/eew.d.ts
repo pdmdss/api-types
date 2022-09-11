3c3fe899a78b2322043f5d9c5857365bbc1bf9e0
soshi1822 <soshi1822@gmail.com>
2022-09-11 13:10:54 +0900

    change: 定義を Lpgm から Lg に変更する
    Signed-off-by: soshi1822 <soshi1822@gmail.com>

--- types/components/eew.d.ts
+++ types/components/eew.d.ts
@@ -1,6 +1,6 @@
 export namespace Eew {
   export type IntensityClass = '0' | '1' | '2' | '3' | '4' | '5-' | '5+' | '6-' | '6+' | '7';
-  export type LpgmIntensityClass = '0' | '1' | '2' | '3' | '4';
+  export type LgIntensityClass = '0' | '1' | '2' | '3' | '4';
 
   export interface Coordinate {
     latitude: {
@@ -70,20 +70,20 @@ export namespace Eew {
     to: IntensityClass | 'over' | '不明';
   }
 
-  export interface IntensityForecastMaxLpgmInt {
-    from: LpgmIntensityClass | '不明';
-    to: LpgmIntensityClass | 'over' | '不明';
+  export interface IntensityForecastMaxLgInt {
+    from: LgIntensityClass | '不明';
+    to: LgIntensityClass | 'over' | '不明';
   }
 
   export interface IntensityAppendix {
     maxIntChange: '0' | '1' | '2';
-    maxLpgmIntChange?: '0' | '1' | '2';
+    maxLgIntChange?: '0' | '1' | '2';
     maxIntChangeReason: '0' | '1' | '2' | '3' | '4' | '9';
   }
 
   export interface Intensity {
     forecastMaxInt: IntensityForecastMaxInt;
-    forecastMaxLpgmInt?: IntensityForecastMaxLpgmInt;
+    forecastMaxLgInt?: IntensityForecastMaxLgInt;
     appendix?: IntensityAppendix;
   }
 
