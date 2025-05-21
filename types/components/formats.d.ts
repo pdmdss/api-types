96f467c9921c4620c68fff05b74f3685cfdc2e35
soshi1822 <soshi1822@gmail.com>
2025-05-21 23:48:38 +0900

    add: 地上気象観測1分値データのフォーマット形式定義を追加
    Signed-off-by: soshi1822 <soshi1822@gmail.com>

--- types/components/formats.d.ts
+++ types/components/formats.d.ts
@@ -1,14 +1,2 @@
 export type DataFormats = 'xml' | 'a/n' | 'binary' | 'grib' | 'bufr' | 'json' | 'shift-jis' | 'jis';
-export type JmaFileFormat =
-  'xml'
-  | 'grib2'
-  | 'bufr3'
-  | 'bufr4'
-  | 'bpf'
-  | 'shp'
-  | 'shx'
-  | 'pdf'
-  | 'png'
-  | 'jpeg'
-  | 'gif'
-  | 'concat';
+export type JmaFileFormat = 'xml' | 'grib2' | 'bufr3' | 'bufr4' | 'jmasf' | 'bpf' | 'shp' | 'shx' | 'pdf' | 'png' | 'jpeg' | 'gif' | 'concat';
