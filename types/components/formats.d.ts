10cce7276ebd411f95ffc3e4e04b2bf7d2ada6b4
soshi1822 <soshi1822@gmail.com>
2025-08-18 07:50:23 +0900

    fix: JmaFile の ファイルフォーマット定義を修正
    Signed-off-by: soshi1822 <soshi1822@gmail.com>

--- types/components/formats.d.ts
+++ types/components/formats.d.ts
@@ -1,2 +1,2 @@
 export type DataFormats = 'xml' | 'a/n' | 'binary' | 'grib' | 'bufr' | 'json' | 'shift-jis' | 'jis';
-export type JmaFileFormat = 'xml' | 'grib2' | 'bufr3' | 'bufr4' | 'jmasf' | 'bpf' | 'shp' | 'shx' | 'pdf' | 'png' | 'jpeg' | 'gif' | 'concat';
+export type JmaFileFormat = 'xml' | 'grib2' | 'bufr3' | 'bufr4' | 'jmasf' | 'bpf' | 'shp' | 'shx' | 'dbf' | 'pdf' | 'png' | 'jpeg' | 'gif' | 'concat' | 'tar';
